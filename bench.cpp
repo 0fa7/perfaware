d4348f2338052c0a6be0b305e2af6312882bc75a
0fa7 <7063015+0fa7@users.noreply.github.com>
2025-09-10 09:59:17 -0400

    start of simd lecture

--- bench.cpp
+++ bench.cpp
@@ -2,6 +2,7 @@
 
 #include "waste.h"
 #include "instructions_per_clock.h"
+#include "simd.h"
 
 static void BM_waste(benchmark::State& state, uint32_t count)
 {
@@ -93,6 +94,26 @@ static void BM_octo_scalar_ptr(benchmark::State& state, uint32_t count)
     }
 }
 
+static void BM_single_sse(benchmark::State& state, uint32_t count)
+{
+    auto input = init_array(count);
+
+    for (auto _ : state)
+    {
+        single_sse(count, input.data());
+    }
+}
+
+static void BM_single_avx(benchmark::State& state, uint32_t count)
+{
+    auto input = init_array(count);
+
+    for (auto _ : state)
+    {
+        single_avx(count, input.data());
+    }
+}
+
 // Register the function as a benchmark
 BENCHMARK_CAPTURE(BM_waste, single_scalar_4096, 4096)->Iterations(1);
 BENCHMARK_CAPTURE(BM_unroll_2_scalar, unroll_2_scalar_4096, 4096)->Iterations(1);
@@ -103,6 +124,23 @@ BENCHMARK_CAPTURE(BM_quad_scalar, quad_scalar_4096, 4096)->Iterations(1);
 BENCHMARK_CAPTURE(BM_quad_scalar_ptr, quad_scalar_ptr_4096, 4096)->Iterations(1);
 BENCHMARK_CAPTURE(BM_octo_scalar, octo_scalar_4096, 4096)->Iterations(1);
 BENCHMARK_CAPTURE(BM_octo_scalar_ptr, octo_scalar_ptr_4096, 4096)->Iterations(1);
+BENCHMARK_CAPTURE(BM_single_sse, single_sse_4096, 4096)->Iterations(1);
+BENCHMARK_CAPTURE(BM_single_avx, single_avx_4096, 4096)->Iterations(1);
 
 // Run the benchmark
-BENCHMARK_MAIN();
+//BENCHMARK_MAIN();
+
+int main(int argc, char** argv) {
+    benchmark::MaybeReenterWithoutASLR(argc, argv);
+    char arg0_default[] = "benchmark";
+    char* args_default = reinterpret_cast<char*>(arg0_default);
+    if (!argv) {
+        argc = 1;
+        argv = &args_default;
+    }
+    ::benchmark::Initialize(&argc, argv);
+    if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+    ::benchmark::RunSpecifiedBenchmarks();
+    ::benchmark::Shutdown();
+    return 0;
+}
---
+++ simd.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <cstdint>
+#include <immintrin.h>
+
+uint32_t __attribute__((target("ssse3"))) single_sse(uint32_t count, uint32_t *input)
+{
+    __m128i sum = _mm_setzero_si128();
+
+    for (int ix = 0; ix < count; ix += 4)
+    {
+        sum = _mm_add_epi32(sum, _mm_load_si128((__m128i*)&input[ix]));
+    }
+
+    // a_1 a_2 a_3 a_4
+    // b_1 b_2 b_3 b_4
+    // a1 + a2, a3 + a4, b_1 + b_2, b_3 + b_4, 0, 0
+
+    // a1 + a2, a3 + a4, b_1 + b_2, b_3 + b_4
+    // a1 + a2, a3 + a4, b_1 + b_2, b_3 + b_4
+    // a1 + a2 + a3 + a4, b_1 + b_2 + b_3 + b_4, 0, 0
+    sum = _mm_hadd_epi32(sum, sum);
+    sum = _mm_hadd_epi32(sum, sum);
+
+    return _mm_cvtsi128_si32(sum);
+}
+
+uint32_t __attribute__((target("avx2"))) single_avx(uint32_t count, uint32_t *input)
+{
+    __m256i sum = _mm256_setzero_si256();
+
+    for (int ix = 0; ix < count; ix += 8)
+    {
+        sum = _mm256_add_epi32(sum, _mm256_loadu_si256((__m256i *)&input[ix]));
+    }
+
+    sum = _mm256_hadd_epi32(sum, sum);
+    sum = _mm256_hadd_epi32(sum, sum);
+
+    __m256i sum_s = _mm256_permute2x128_si256(sum, sum, 1 | 1 << 4);
+
+    sum = _mm256_hadd_epi32(sum, sum_s);
+
+    return _mm256_cvtsi256_si32(sum);
+}
+
